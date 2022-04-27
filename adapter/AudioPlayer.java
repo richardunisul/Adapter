943fa47bd61b32d561e1e372fab40d4104a07708
richardunisul <103585524+richardunisul@users.noreply.github.com>
2022-04-27 13:38:38 -0300

    Add files via upload

---
+++ adapter/AudioPlayer.java
@@ -0,0 +1,21 @@
+package adapter;
+
+public class AudioPlayer implements MediaPlayer{
+	MediaAdapter mediaAdapter; 
+	
+	@Override
+	public void play(String audioType, String fileName) {
+			//inbuilt suppot to play mp3 music files
+		if(audioType.equalsIgnoreCase("mp3")) {
+			System.out.println("Playing mp3 file. Name "+ fileName);
+		}
+		//mediaAdapter is providing support to play other files formats
+		else { if(audioType.equalsIgnoreCase("vlc") || audioType.equalsIgnoreCase("mp4")) {
+			mediaAdapter = new MediaAdapter(audioType);
+			mediaAdapter.play(audioType, fileName);
+		} else {
+			System.out.println("Invalid media. " + audioType + " format not supported");
+		}
+		}
+	}
+}
